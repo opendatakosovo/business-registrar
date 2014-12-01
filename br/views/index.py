8f10d02bb125d2d2604c0262cd465b89b00fcfce
endrit-b <endriti.92@gmail.com>
2014-12-01 15:50:08 +0100

    business form is saving in mongodb

--- br/views/index.py
+++ br/views/index.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from br.views.forms.indexform import IndexForm
 from flask.views import MethodView
+from br import mongo
+from br.utils.utils import Utils
+
+utils = Utils()
 
 
 class Index(MethodView):
@@ -15,5 +19,22 @@ class Index(MethodView):
     def post(self):
         '''process data and save them in database
         '''
-        form = IndexForm()
-        return render_template('index.html', form=form)
+
+        #get the doc ID from utils
+        doc_id = utils.get_doc_id()
+        #save business form
+        self.save_business_form(doc_id)
+
+        return redirect(url_for('index'))
+
+    def save_business_form(self, doc_id):
+
+        # Update the patient doc with treatment.
+        business_form = IndexForm(request.form)
+        business_registrar = business_form.data
+
+        mongo.db.business.update(
+            {'_id': doc_id},
+            {'$set': {'businessRegistration': business_registrar}},
+            True
+        )
